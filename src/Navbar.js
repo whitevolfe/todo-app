1673ec49f03ee1745b95487702138dc33da856a8
whitevolfe <azeemshanr@gmail.com>
2021-12-16 11:24:10 +0530

    Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>The Software Blog</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+
+            </div>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
